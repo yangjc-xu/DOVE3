02e64cfd79289be5b73dd300b54908643abfdf15
Poutine1025 <yangjc@live.unc.edu>
2022-09-23 13:45:58 -0400

    Update exampleData.R

--- R/exampleData.R
+++ R/exampleData.R
@@ -11,12 +11,12 @@
 #'   The data.frame contains 11 columns,
 #'   \describe{
 #'   \item{subject.id}{The subject ID of each record.}
-#'   \item{event.time}{The event time in days.}
+#'   \item{event.time}{The event (infection, hospitalization or death) time in days. Infection is treated as recurrent event.}
 #'   \item{censor.time}{The censoring time for each subject in days.}
 #'   \item{entry.time}{The entry time in days.}
 #'   \item{Vtime}{The time of vaccination/booster in days.}
 #'   \item{Vtype}{The type of vaccination/booster.}
-#'   \item{infection.time}{The time of infection in days.}
+#'   \item{infection.time}{The time of infection in days. If the outcome is infection, infection.time is the same as event.time.}
 #'   \item{infection.type}{The type of infection.}
 #'   \item{age}{A categorical variable of age (<18, 18-34, 35-49, 50-64, >=65).}
 #'   \item{gender}{A binary indicator of gender (male/female).}
